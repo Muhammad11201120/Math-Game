d0241e550c3f9fd4187b729f1d25b2019d05e218
mo <muha.11201120@gmail.com>
2023-02-17 11:19:10 +0300

    write pseudo code

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// How Many Questions do you want to answer?
+// questions level 1 easy, 2 med, 3 hard, 4 mix
+// operations type 1 add, 2 sub, 3 mul, 4 div , 5 mix
+/*************************************************/
+// print question of all [?/?]
+// start questions
+// line seperator
+// result
+// correct answers : green color
+/***********************************************/
+// print question of all [?/?]
+// start questions
+// line seprator
+// result
+// wrong answer : red color | beeb
+// print right answer is:
+/**********************************************/
+// line seprator
+// print final result
+// line seprator
+/**********************************************/
+// number of questions:
+// question leve:
+// opreation type:
+// number of right answers:
+// number of wrong answers:
+// line seprator
+// Do you want to play again?
+int main()
+{
+    return 0;
+}
